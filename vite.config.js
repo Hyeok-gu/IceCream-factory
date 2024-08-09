19e34c75544842723db5463b7748b57e9250b848
Hyeok-gu <thanatos6737@naver.com>
2024-08-09 10:18:59 +0900

    경로 변경

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hyeok-gu.github.io/home",
+  base: "/hyeok-gu.github.io/",
 });
