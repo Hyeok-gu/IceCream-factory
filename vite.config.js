33aabcf692fcf8066c96661022e89bb0f1be8c8a
Hyeok-gu <thanatos6737@naver.com>
2024-08-09 10:58:41 +0900

    빌드

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/hyeok-gu.github.io/",
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // 1000 KB로 경고 한도 증가
   },
