48df928cfc0a65574b2a3f8f51915e3983d70981
Hyeok-gu <thanatos6737@naver.com>
2024-08-15 13:33:02 +0900

    새로고침 에러 보완 테스트

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://hyeok-gu.github.io/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // 1000 KB로 경고 한도 증가
