d7c39e2aa697f46703ff255c4efc9c101d9ffa5f
Hyeok-gu <thanatos6737@naver.com>
2024-08-19 12:58:53 +0900

    경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
     chunkSizeWarningLimit: 1000, // 1000 KB로 경고 한도 증가
   },
